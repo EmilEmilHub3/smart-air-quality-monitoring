86ca5a55e8395222f9075097571f22bf1bb762e3
EmilEmilHub3 <emiros01@easv365.dk>
2026-06-09 20:22:41 +0200

    Delete dist directory

--- dist/src/measurements/measurements.gateway.d.ts
+++
@@ -1,6 +0,0 @@
-import { Server } from 'socket.io';
-import { Measurement } from './measurement.entity';
-export declare class MeasurementsGateway {
-    server: Server;
-    sendNewMeasurement(measurement: Measurement): void;
-}
